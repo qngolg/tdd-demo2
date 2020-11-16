013965b042dba19020028290d49fd8a636d24852
qgl <qgl@qgldeMac-mini.local>
2020-11-16 15:30:49 +0800

    test

--- src/main/java/com/test/game/TestMain.java
+++ src/main/java/com/test/game/TestMain.java
@@ -1,8 +1,21 @@
 package com.test.game;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TestMain {
 
     public static void main(String[] args) {
-
+        User user =new User();
+        user.setName("qgl");
+        Gson gson = new Gson();
+        String path = "a.json";
+        try {
+            gson.toJson(user,new FileWriter(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
